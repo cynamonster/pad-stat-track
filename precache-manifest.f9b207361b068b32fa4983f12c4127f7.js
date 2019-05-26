9352d86f7f4fd3f930af9bbb708eda00b9baf9dd
Ben Cynamon <ben.cynamon@gmail.com>
2019-05-26 13:12:34 -0400

    Updates

---
+++ precache-manifest.f9b207361b068b32fa4983f12c4127f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0f6e6b761e884ab22dd",
+    "url": "/pad-stat-track/static/css/main.177a2c54.chunk.css"
+  },
+  {
+    "revision": "e0f6e6b761e884ab22dd",
+    "url": "/pad-stat-track/static/js/main.a1011f2b.chunk.js"
+  },
+  {
+    "revision": "5306904edbd31ae3c580",
+    "url": "/pad-stat-track/static/js/runtime~main.2a340835.js"
+  },
+  {
+    "revision": "040f73fc680f0081a18a",
+    "url": "/pad-stat-track/static/css/2.855eb1bd.chunk.css"
+  },
+  {
+    "revision": "040f73fc680f0081a18a",
+    "url": "/pad-stat-track/static/js/2.8005a7d5.chunk.js"
+  },
+  {
+    "revision": "d0d14b1da1aa1b8f864e2118d4f6ca0f",
+    "url": "/pad-stat-track/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pad-stat-track/precache-manifest.ac451a127c67292a77f00ba02392767a.js"
+  "/pad-stat-track/precache-manifest.f9b207361b068b32fa4983f12c4127f7.js"
 );
 
 workbox.clientsClaim();
